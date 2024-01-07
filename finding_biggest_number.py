d72427796b341c4158a99595e4b141bf83cbffb3
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-07 08:30:21 +0700

    label for num 3

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -48,6 +48,9 @@ label_num1.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
 label_num2 = ttk.Label(frame, text="Enter second number:", font=("Georgia", 16))
 label_num2.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
 
+label_num3 = ttk.Label(frame, text="Enter third number:", font=("Georgia", 16))
+label_num3.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
+
 # Button to find the largest number
 # Label to display the result
 # Styling the button
